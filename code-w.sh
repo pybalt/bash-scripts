090c85dea05b454d45a175cb493c371e030f6e49
Leonel B. Bravo <leobravolbb2002bej@gmail.com>
2022-11-29 18:41:22 -0300

    updated for VSCode profiles

--- code-w.sh
+++ code-w.sh
@@ -1,17 +1,17 @@
-## This opens VSCode with a predefined workspace.
+## This opens VSCode with a predefined profile.
 
 if [[ $1 == rust ]] || [[ $1 == c ]] || [[ $1 == c++ ]] ; then
-    $(code ~/devspace/rust_c++.code-workspace)
+    $(code-insiders --profile Rust)
 fi
 
 if [[ $1 == python ]] || [[ $1 == py ]] ; then
-    $(code ~/devspace/python.code-workspace)
+    $(code-insiders --profile Python)
 fi
 
 if [[ $1 == js-ts ]] || [[ $1 == js ]] || [[ $1 == ts ]] ; then
-    $(code ~/devspace/js_ts.code-workspace)
+    $(code-insiders --profile TypeScript)
 fi
 
 if [[ $1 == java ]] ; then
-    $(code ~/devspace/java.code-workspace)
+    $(code-insiders --profile Java)
 fi
