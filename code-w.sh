d30ec9c6d07967e4a6bbea2aa201a7f491801978
Leonel B. Bravo <leobravolbb2002bej@gmail.com>
2022-11-29 19:34:20 -0300

    feature: allowing second paramenter

--- code-w.sh
+++ code-w.sh
@@ -1,17 +1,19 @@
+#!/bin/sh
 ## This opens VSCode with a predefined profile.
 
+
 if [[ $1 == rust ]] || [[ $1 == c ]] || [[ $1 == c++ ]] ; then
-    $(code-insiders --profile Rust)
+    $(code-insiders --profile Rust $2)
 fi
 
 if [[ $1 == python ]] || [[ $1 == py ]] ; then
-    $(code-insiders --profile Python)
+    $(code-insiders --profile Python $2)
 fi
 
 if [[ $1 == js-ts ]] || [[ $1 == js ]] || [[ $1 == ts ]] ; then
-    $(code-insiders --profile TypeScript)
+    $(code-insiders --profile TypeScript $2)
 fi
 
 if [[ $1 == java ]] ; then
-    $(code-insiders --profile Java)
+    $(code-insiders --profile Java $2)
 fi
